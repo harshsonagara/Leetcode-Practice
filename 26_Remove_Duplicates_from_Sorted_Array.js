0988dbf53823c9aabff095483b0934d6646d91fb
harshsonagara <harshsonagara0@gmail.com>
2025-06-14 11:54:24 +0530

    remove duplicate

--- 26_Remove_Duplicates_from_Sorted_Array.js
+++ 26_Remove_Duplicates_from_Sorted_Array.js
@@ -1,13 +1,14 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var removeDuplicates = function (nums) {
-    let j = 0;
-    for (let i = 1; i < nums.length; i++) {
-
-        if (nums[i] !== nums[j]) {
+    let j = 1;
+    for (let i = 0; i < nums.length-1; i++) {
+        if (nums[i] !== nums[i+1]) {
+            nums[j] = nums[i+1]
             j++;
-            nums[j] = nums[i]
-
         }
     }
-    return j + 1;
+    return j;
 };
