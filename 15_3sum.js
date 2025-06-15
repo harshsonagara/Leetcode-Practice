d689b38fcd129a512828b83b07fbcb8cc51b6535
harshsonagara <harshsonagara0@gmail.com>
2025-06-15 13:47:29 +0530

    3sum optimal approach

--- 15_3sum.js
+++ 15_3sum.js
@@ -18,3 +18,24 @@ var threeSum = function (nums) {
     }
     return arr
 };
+
+var threeSum = function (nums) {
+    nums.sort((a,b)=>a-b);
+    let ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i != 0 && nums[i - 1] == nums[i]) continue;
+        let j = i + 1, k = nums.length - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                let cur = [nums[i], nums[j++], nums[k--]]
+                ans.push(cur);
+
+                while (j < k && nums[j - 1] == nums[j]) j++;
+                while (j < k && nums[k + 1] == nums[k]) k--;
+            } else if (sum < 0) j++;
+            else k--;
+        }
+    }
+    return ans;
+};
