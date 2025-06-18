6fd4abf9343692e35d920bb341d912a08fc8a97d
harshsonagara <harshsonagara0@gmail.com>
2025-06-18 17:46:25 +0530

    Binary search completed

--- 704_binary_search.js
+++ 704_binary_search.js
@@ -4,8 +4,15 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if(target == nums[i]) return i ;
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
+
     return -1;
 };
