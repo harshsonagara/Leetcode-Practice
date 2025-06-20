815df7d13625a89274d3187d21e13ff680636daf
harshsonagara <harshsonagara0@gmail.com>
2025-06-20 23:07:00 +0530

    continuous array

--- 525_contiguous_array.js
+++ 525_contiguous_array.js
@@ -6,7 +6,7 @@ var findMaxLength = function (nums) {
     let map = new Map();
     let sum = 0;
     let maxlength = 0;
-    map.set(0, -1);
+    map.set(sum, -1);
     for (let i = 0; i < nums.length; i++) {
         if(nums[i] == 0) nums[i] = -1;
         sum += nums[i]
