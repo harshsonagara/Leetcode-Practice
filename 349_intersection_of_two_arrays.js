ffac8b0496f8732861cb950b6a1aa7b1a7187302
harshsonagara <harshsonagara0@gmail.com>
2025-06-20 16:45:33 +0530

    written for O(1) and O(n)

--- 349_intersection_of_two_arrays.js
+++ 349_intersection_of_two_arrays.js
@@ -11,3 +11,15 @@ var intersection = function (nums1, nums2) {
     }
     return ans;
 };
+
+// written for O(1) and O(n)
+var intersection = function (nums1, nums2) {
+    let set = new Set(nums1);
+    let resultSet = new Set();
+
+    for (let i = 0; i < nums2.length; i++) {
+        if (set.has(nums2[i])) resultSet.add(nums2[i]);
+    }
+
+    return Array.from(resultSet);
+};
