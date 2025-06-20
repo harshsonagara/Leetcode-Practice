9ec3e04e9834e803df46b1931e4086ca4afe0e5c
harshsonagara <harshsonagara0@gmail.com>
2025-06-20 18:02:50 +0530

    optimal approach

--- 560_subarray_sum_equals_k.js
+++ 560_subarray_sum_equals_k.js
@@ -17,3 +17,22 @@ var subarraySum = function (nums, k) {
     }
     return count;
 };
+
+// ** Optimal Approach */
+
+
+var subarraySum = function (nums, k) {
+     let count = 0, sum = 0;
+    let map = new Map();
+    map.set(sum, 1); 
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (map.has(sum - k)) {
+            count += map.get(sum - k);
+        }
+        map.set(sum, (map.get(sum) || 0) + 1);
+    }
+
+    return count;
+};
